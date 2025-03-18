d96288f7d51767c4902c5f3325d3fa9b47cdc545
bt-soft <bt-soft@freemail.hu>
2025-03-18 21:29:20 +0100

    Update screen saver timeout to 5 minutes for improved user experience

--- pico-radio.ino
+++ pico-radio.ino
@@ -206,7 +206,7 @@ void loop() {
         // Aktuális Display loopja
         bool touched = pDisplay->loop(encoderState);
 
-#define SCREEN_SAVER_TIME 1000 * 60 * 1  // 5 perc
+#define SCREEN_SAVER_TIME 1000 * 60 * 5  // 5 perc a képernyővédő időzítése
         static uint32_t lastScreenSaver = millis();
         // Ha volt touch valamelyik képernyőn, vagy volt rotary esemény...
         // Volt felhasználói interakció?
